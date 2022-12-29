7f8ccc7b794a9e8f2168b0efd5b712a9e24aae0b
kobo-cash <73577274+kobo-cash@users.noreply.github.com>
2022-12-29 15:02:50 +0300

    Add files via upload

---
+++ vulCode4.py
@@ -0,0 +1,16 @@
+from flask import request
+import pickle
+import yaml
+
+@app.route('/pickle')
+def pickle_loads():
+    file = request.files['pickle']
+    # deepcode ignore Deserialization: <please specify a reason of ignoring this>
+    pickle.load(file) # Noncompliant; Never use pickle module to deserialize user inputs
+
+@app.route('/yaml')
+def yaml_load():
+    data = request.GET.get("data")
+    yaml.load(data, Loader=yaml.Loader) # Noncompliant; Avoid using yaml.load with unsafe yaml.Loader
+
+
